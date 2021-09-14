b372fc5c9c9141298c18da5a389f6d49e0610052
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-09-14 13:21:44 +0300

    Improved useGames hook

--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -1,51 +1,34 @@
 import { useMemo } from 'react';
 
 export const useSortedGames = (games, sort, tags, minPrice) => {
-  const sortedGames = useMemo(() => {
-    if (minPrice && tags.length) {
-      const minPricedGames = games.filter((game) => parseFloat(game.price) > minPrice);
-      return minPricedGames.filter((game) => game.tags.some((tag) => tags.includes(tag)));
-    }
+  return useMemo(() => {
+    let sortedGames = games;
 
     if (minPrice) {
-      return games.filter((game) => parseFloat(game.price) > minPrice);
-    }
-
-    if (sort === 'title' && tags.length) {
-      const sortedGames = [...games].sort((a, b) => a[sort].localeCompare(b[sort]));
-      return sortedGames.filter((game) => game.tags.some((tag) => tags.includes(tag)));
-    }
-
-    if (sort === 'price' && tags.length) {
-      const sortedGames = [...games].sort((a, b) =>
-        parseFloat(a[sort]) - parseFloat(b[sort]));
-      return sortedGames.filter((game) => game.tags.some((tag) => tags.includes(tag)));
+      sortedGames = sortedGames.filter((game) => parseFloat(game.price) > minPrice);
     }
 
     if (sort === 'title') {
-      return [...games].sort((a, b) => a[sort].localeCompare(b[sort]));
+      sortedGames = [...sortedGames].sort((a, b) => a[sort].localeCompare(b[sort]));
     }
 
     if (sort === 'price') {
-      return [...games].sort((a, b) => parseFloat(a[sort]) - parseFloat(b[sort]));
+      sortedGames = [...sortedGames].sort((a, b) => parseFloat(a[sort]) - parseFloat(b[sort]));
     }
 
     if (tags.length) {
-      return games.filter((game) => game.tags.some((tag) => tags.includes(tag)));
+      sortedGames = sortedGames.filter((game) => game.tags.some((tag) => tags.includes(tag)));
     }
 
-    return games;
-  }, [sort, games, tags, minPrice]);
-  return sortedGames;
+    return sortedGames;
+  }, [games, sort, tags, minPrice]);
 };
 
 export const useGames = ({ games, sort, tags, minPrice, query }) => {
   const sortedGames = useSortedGames(games, sort, tags, minPrice);
 
-  const fullFilteredGames = useMemo(()=> {
+  return useMemo(() => {
     return sortedGames.filter((game) =>
       game.title.toLowerCase().includes(query.toLowerCase()));
   }, [query, sortedGames]);
-
-  return fullFilteredGames;
 };
